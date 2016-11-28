b0d8c6c23f89616bd7dbe8029452b5f3e23f1c63
houzizhu1991 <836569587@qq.com>
2016-11-28 14:06:18 +0800

    add new file

---
+++ NativePod/Classes/NewModel.h
@@ -0,0 +1,13 @@
+//
+//  NewModel.h
+//  Pods
+//
+//  Created by 世纪阳天 on 2016/11/28.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewModel : NSObject
+
+@end
---
+++ NativePod/Classes/NewModel.m
@@ -0,0 +1,13 @@
+//
+//  NewModel.m
+//  Pods
+//
+//  Created by 世纪阳天 on 2016/11/28.
+//
+//
+
+#import "NewModel.h"
+
+@implementation NewModel
+
+@end
